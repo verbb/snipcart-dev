010e380629462b0d0954fd56541048a4287294a3
Matt Stein <matt@workingconcept.com>
2020-01-13 18:54:42 -0600

    Update migration.
    Required for 3.4.0-beta.5.

--- migrations/m191102_142109_test_graphql.php
+++ migrations/m191102_142109_test_graphql.php
@@ -5,6 +5,7 @@ namespace craft\contentmigrations;
 use Craft;
 use craft\db\Migration;
 use craft\models\GqlSchema;
+use craft\models\GqlToken;
 
 /**
  * m191102_142109_test_graphql migration.
@@ -16,15 +17,26 @@ class m191102_142109_test_graphql extends Migration
      */
     public function safeUp()
     {
-        $testSchema = new GqlSchema();
+        $graphQlService = Craft::$app->getGql();
 
-        $testSchema->name = 'Test Schema';
-        $testSchema->enabled = true;
-        $testSchema->expiryDate = null;
-        $testSchema->accessToken = 'SUPERSECRETTESTTOKEN';
-        $testSchema->scope = $this->_getSchemaScope();
+        // create schema + scope
+        $schema = new GqlSchema();
 
-        Craft::$app->getGql()->saveSchema($testSchema);
+        $schema->name = 'Test Schema';
+        $schema->scope = $this->_getSchemaScope();
+
+        $graphQlService->saveSchema($schema);
+
+        // create token
+        $token = new GqlToken();
+
+        $token->name = 'Test Token';
+        $token->accessToken = 'SUPERSECRETTESTTOKEN';
+        $token->enabled = true;
+        $token->expiryDate = null;
+        $token->schemaId = $schema->id;
+
+        $graphQlService->saveToken($token);
 
         // for GraphQL feature
         Craft::$app->setEdition(Craft::Pro);
