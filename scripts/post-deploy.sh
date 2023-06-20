9f71f1213bd224d790bd266ced9a9bdbcdd5e5f4
Stephen Callender <stephen@fostercommerce.com>
2023-06-20 19:55:04 -0400

    Disable two post-deploy commands

--- scripts/post-deploy.sh
+++ scripts/post-deploy.sh
@@ -5,6 +5,6 @@ cd "$SITE_PATH"
 
 ./craft migrate/all --no-backup
 ./craft project-config/apply
-./craft clear-caches/all # Not too bothered about this because it's a plugin dev site
+# ./craft clear-caches/all # Not too bothered about this because it's a plugin dev site
 
-sudo -S service php8.0-fpm reload
+# sudo -S service php8.1-fpm reload
