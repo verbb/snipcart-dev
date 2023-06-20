a277dcff1d291135aed7feb15b5bcaa3f17ee43e
Stephen Callender <stephen@fostercommerce.com>
2023-06-20 16:54:33 -0400

    Update craft files

--- web/index.php
+++ web/index.php
@@ -3,10 +3,19 @@
  * Craft web bootstrap file
  */
 
-// Load shared bootstrap
-require dirname(__DIR__) . '/bootstrap.php';
+// Set path constants
+define('CRAFT_BASE_PATH', dirname(__DIR__));
+const CRAFT_VENDOR_PATH = CRAFT_BASE_PATH . '/vendor';
+
+// Load Composer's autoloader
+require_once CRAFT_VENDOR_PATH.'/autoload.php';
+
+// Load dotenv?
+if (class_exists('Dotenv\Dotenv') && file_exists(CRAFT_BASE_PATH.'/.env')) {
+	Dotenv\Dotenv::create(CRAFT_BASE_PATH)->load();
+}
 
 // Load and run Craft
-/** @var craft\web\Application $app */
-$app = require CRAFT_VENDOR_PATH . '/craftcms/cms/bootstrap/web.php';
+define('CRAFT_ENVIRONMENT', getenv('ENVIRONMENT') ?: 'production');
+$app = require CRAFT_VENDOR_PATH.'/craftcms/cms/bootstrap/web.php';
 $app->run();
